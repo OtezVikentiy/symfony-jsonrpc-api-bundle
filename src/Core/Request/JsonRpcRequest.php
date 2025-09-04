ba5b41a883757db4334c88fd1da1bd0f138eb895
Leonid Groshev <OtezVikentiy@gmail.com>
2025-09-04 23:25:49 +0300

    updated documentation

--- src/Core/Request/JsonRpcRequest.php
+++ src/Core/Request/JsonRpcRequest.php
@@ -14,15 +14,19 @@ abstract class JsonRpcRequest
 
         $return = [];
         foreach ($properties as $property) {
-            $getterName = $this->createGetter($property->getName());
+            $getterName = $this->createGetter($property->getName(), $property->getType()->getName());
             $return[$property->getName()] = $this->$getterName();
         }
 
         return $return;
     }
 
-    private function createGetter(string $propertyName): string
+    private function createGetter(string $propertyName, string $propertyType): string
     {
+        if ($propertyType === 'bool' || $propertyType === 'boolean') {
+            return 'is' . ucfirst($propertyName);
+        }
+
         return 'get' . ucfirst($propertyName);
     }
 }
